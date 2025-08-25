dded5b92d16bb8c445cc349738cbba92bcd9aecd
dten111213 <itsdaveten@gmail.com>
2025-08-24 22:52:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class AutomatedTokenManager {
     this.oauth2Client = new google.auth.OAuth2(
       process.env.GOOGLE_ADS_CLIENT_ID,
       process.env.GOOGLE_ADS_CLIENT_SECRET,
-      'urn:ietf:wg:oauth:2.0:oob'
+      'http://localhost:8080/oauth/callback'
     );
 
     this.tokensFile = './tokens.json';
